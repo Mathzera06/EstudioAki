07b81e1bbaf4e5b993051a1fe2fdc93718596c45
Matheus Andrade Lopes <102606784+Mathzera06@users.noreply.github.com>
2023-04-12 22:53:47 -0300

    create_db.sql
    Criada as outras tabelas do banco. Será realizado ajustes na mesma.

--- create_db.sql
+++ create_db.sql
@@ -9,5 +9,29 @@ CREATE TABLE users (
     password varchar(255) not null
 );
 
+CREATE TABLE tb_instrumentos (
+id_instrumentos int primary key auto_increment,
+id_estudio int,
+instrumento varchar(200),
+descricao varchar(200)
+)
+
+CREATE TABLE tb_agenda (
+id_agenda int primary key auto_increment,
+datas_estudios timestamp,
+id_estudio int
+)
+
+CREATE TABLE tb_estudio(
+id_estudio int primary key auto_increment,
+nome_estudio varchar(40),
+endereco varchar(100),
+logradouro varchar(100),
+bairro varchar(100),
+estado varchar(50),
+datas_estudio timestamp
+)
+
+
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
 flush privileges;
