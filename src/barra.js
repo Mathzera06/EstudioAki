b0c09945e480de9753fef36571140a978630a8fa
‘carlos’ <carlos.diegoticona@yahoo.com>
2023-04-19 18:53:03 -0300

    Esqueleto do front

---
+++ src/barra.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function barra() {
+  return (
+    <div>
+      <nav style={{ backgroundColor: "#000000", height: "50px" }}>
+        inicio
+      </nav>
+      
+    </div>
+  );
+}
+
+export default barra;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
-import React from 
-'react';
+import { render } from '@testing-library/react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import barra from './barra';
 
-const App = () => {
-  return (
-    <div>
-      teste
+class App extends React.Component{
+
+  componentDidMount() {
+    document.body.style.backgroundColor ='#6495ED';
+  }
 
 
 
-    </div>
-  )
+  icones = {
+
+    'Inverno': 'fa-snowman'
+  }
+
+
   
+//'#4169E1'
+
+render(){
+  return(
+    
+    <div style={{margin: 'auto', width: "inline-block", backgroundColor: 'black',color:'white', padding:12, borderRadius: 8}}> 
+     <p className='white-text'>AQUI FICARIAM OS BOTÔES DE NAVEGAÇÃO</p>
+    </div>
+  
+  )
 }
+}
+
 
 ReactDOM.render(
   <App />,
