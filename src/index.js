98c86ad78d6628c259985f806f6d57e41066660e
rinumacm <128430557+rinumacm@users.noreply.github.com>
2023-04-19 16:22:28 -0300

    inicio do esqueleto front

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 
+'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      teste
+
+
+
+    </div>
+  )
+  
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector("#root")
+)  
+
+
