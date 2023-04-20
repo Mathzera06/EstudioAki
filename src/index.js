b81e1839935990a0b82755efc436e3710ede4d8c
rinumacm <128430557+rinumacm@users.noreply.github.com>
2023-04-20 12:50:41 -0300

    começando o navbar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import { render } from '@testing-library/react';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import barra from './barra';
+import Logosa from './componentes/imgs/Logo.png'
+import './index.css'
 
-class App extends React.Component{
+class App extends React.Component {
 
   componentDidMount() {
-    document.body.style.backgroundColor ='#6495ED';
+    document.body.style.backgroundColor = '#0E243B';
   }
 
 
@@ -17,24 +18,46 @@ class App extends React.Component{
   }
 
 
-  
-//'#4169E1'
 
-render(){
-  return(
-    
-    <div style={{margin: 'auto', width: "inline-block", backgroundColor: 'black',color:'white', padding:12, borderRadius: 8}}> 
-     <p className='white-text'>AQUI FICARIAM OS BOTÔES DE NAVEGAÇÃO</p>
-    </div>
-  
+  //'#4169E1'
+
+  //   render() {
+  //     return (
+  //       // <img src={Logosa}/>
+
+  //       <div style={{
+  //         margin: 'auto', width: "100%", padding: "35px 0",
+  //         alignItems: 'center', justifyContent: "space-between" ,backgroundColor: '#546CCF',
+  //         color: 'white', padding: 12, borderRadius: 8
+  //       }}>
+  //         {/* <img src={Logosa} /> */}
+  //         <p className='white-text'>AQUI FICARIAM OS BOTÔES DE NAVEGAÇÃO</p>
+  //       </div>
+
+  //     )
+  //   }
+  // }
+
+  render() {
+    return (
+      <div className='navbar'>
+        <img src={Logosa} className='logo' />
+        <ul>
+          <li><a href="#">home</a></li>
+          <li><a href="#">home</a></li>
+          <li><a href="#">home</a></li>
+          <li><a href="#">home</a></li>
+          <li><a href="#">home</a></li>
+        </ul>
+      </div>
   )
-}
+  }
 }
 
 
 ReactDOM.render(
   <App />,
   document.querySelector("#root")
-)  
+)
 
 
