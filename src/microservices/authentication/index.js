352c54b461a407cdbe065e6f9cbc6c72a8fe3841
Bruno Silva <brunosco2002@gmail.com>
2023-04-23 13:42:17 -0300

    remove refresh token

--- src/microservices/authentication/index.js
+++ src/microservices/authentication/index.js
@@ -53,9 +53,8 @@ app.post('/login', async (req, res) => {
         user = rest;
 
         const accessToken = generateAccessToken(user);
-        const refreshToken = await generateRefreshToken(user);
 
-        return res.json({ accessToken, refreshToken });
+        return res.json({ accessToken });
     }
 
     return res.send('Usuário não encontrado', 400);
@@ -71,43 +70,11 @@ app.delete('/logout', async (req, res) => {
     res.sendStatus(204);
 });
 
-app.post('/generate_token', async (req, res) => {
-    const refreshToken = req.body.refreshToken;
-    if (!refreshToken) return res.sendStatus(401);
-
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET_KEY, (error, tokenData) => {
-        if (error) return res.sendStatus(403);
-        req.user = tokenData;
-    });
-
-    const hasRefreshToken = await RefreshToken.findOne({
-        where: {
-            key: refreshToken,
-            userId: req.user.id
-        }
-    });
-    if (!hasRefreshToken) return res.sendStatus(403);
-
-    return res.json({
-        accessToken: generateAccessToken(req.user)
-    });
-
-});
 
 function generateAccessToken(user) {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET_KEY, { expiresIn: '10m' });
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET_KEY);
 }
 
-async function generateRefreshToken(user) {
-    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET_KEY);
-
-    await RefreshToken.create({
-        key: refreshToken,
-        userId: user.id
-    });
-
-    return refreshToken;
-}
 
 app.listen(3000, () => {
     console.log("Authentication is running. Port 3000")
