6fa633ebe7dea72a6ef601fde7a17f32394b486f
Bruno Silva <brunosco2002@gmail.com>
2023-05-19 22:23:44 -0300

    fix errro

--- back-end/src/microservices/authentication/middleware.js
+++ back-end/src/microservices/authentication/middleware.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 
 function jwtAuthentication(req, res, next) {
     const authorization = req.headers.authorization;
+    if (!authorization) return res.sendStatus(403);
+    
     const token = authorization.split(' ')[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (error, tokenData) => {
         if (error) return res.sendStatus(403);
