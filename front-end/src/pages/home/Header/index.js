75869fd3d280e3fb40ea117799a3ca954b7204de
Bruno Silva <brunosco2002@gmail.com>
2023-05-27 16:10:18 -0300

    add EstudioAki official colors

--- front-end/src/pages/home/Header/index.js
+++ front-end/src/pages/home/Header/index.js
@@ -10,7 +10,7 @@ const Header = () => {
                         Da inspiração à criação
                     </h1>
                     <h2 class="masthead-subheading mt-2 mb-0">alugue o estúdio perfeito agora mesmo</h2>
-                    <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Learn More</a>
+                    <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Encontar Estúdios</a>
                 </div>
             </div>
             <div class="bg-circle-1 bg-circle"></div>
