228e20af35a1bb6a1ff05feb4e0ded5edfb1c529
Bruno Silva <brunosco2002@gmail.com>
2023-06-19 22:08:27 -0300

    studio form and instrument upgrades

--- front-end/src/pages/instruments-register/index.js
+++ front-end/src/pages/instruments-register/index.js
@@ -1,17 +1,19 @@
 // InstrumentRegister.js
 
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
 import { Col, Button, Row, Container, Card, Form, Alert } from 'react-bootstrap';
 import Navigation from '../../components/Navigation';
-import { useParams } from 'react-router-dom';
-import { ArrowLeft } from 'react-feather'
+import { Link, useParams } from 'react-router-dom';
+import { ArrowLeft, ArrowRight, ArrowRightCircle, Check, ChevronRight } from 'react-feather'
+import { toast } from 'react-toastify';
 
 const Instrument_Register = () => {
 
   const { estudio_id } = useParams();
   const estudioId = estudio_id.replace(':', '')
   const [instrumentName, setInstrumentName] = useState('');
+  const [studioInstruments, setStudioInstruments] = useState([]);
   const [instrumentDescription, setInstrumentDescription] = useState('');
   const [errorMsg, setErrorMsg] = useState('');
   const [successMsg, setSuccessMsg] = useState('');
@@ -31,8 +33,8 @@ const Instrument_Register = () => {
         }
       })
       .then((res) => {
-        setSuccessMsg('Instrumento cadastrado com sucesso');
-        // Lógica adicional, se necessário
+        getStudioInstruments();
+        toast.success('Instrumento criado!');
       })
       .catch((error) => {
         console.error(error);
@@ -40,22 +42,58 @@ const Instrument_Register = () => {
       });
   };
 
+  const getStudioInstruments = useCallback(() => {
+    axios.get(`http://localhost:5000/studios/${estudioId}/instruments`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'authorization': `Bearer ${localStorage.getItem('token')}`,
+        'Accept': 'application/json',
+      }
+    }).then(res => {
+      setInstrumentName('');
+      setInstrumentDescription('')
+      setStudioInstruments(res.data);
+    }).catch((error) => {
+      console.error(error);
+      setErrorMsg(error.response?.data);
+
+    });
+  }, [estudioId])
+
+  useEffect(() => {
+    getStudioInstruments();
+  }, [getStudioInstruments])
+
   return (
     <div style={{ background: "#0E243B" }}>
       <Container>
         <Navigation />
-        <Row className=" vh-100 d-flex justify-content-center align-items-center">
+        <Row className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
           <Col md={8} lg={6} xs={12}>
             <Card className="px-4 mt-5">
               <Card.Body>
                 <div className="mb-3 mt-md-4">
-                  <h2 className="fw-bold mb-2 text-center text-uppercase ">
-                    Cadastre seu Equipamento
-                  </h2>
+                  <h3 className="fw-bold mb-3 text-center text-uppercase ">
+                    Cadastre seu <span className='text-primary'>Equipamento</span>
+                  </h3>
+                  <div className='text-center my-3'>
+                    <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
+                      <input type="radio" disabled className="btn-check cursor-normal" name="btnradio" id="btnradio1" />
+                      <label className="btn btn-sm btn-outline-primary cursor-normal btn-disabled" for="btnradio1">
+                        1 - Informações do estúdio
+                      </label>
+
+                      <input type="radio" className="btn-check" name="btnradio" id="btnradio2" checked />
+                      <label className="btn btn-sm btn-outline-primary" for="btnradio2">
+                        2 - Instrumentos
+                      </label>
+                    </div>
+                  </div>
                   <form onSubmit={onSubmitFunction}>
                     <Form.Group>
                       <Form.Label className='mb-2 '>Nome do Instrumento</Form.Label>
                       <Form.Control
+                        required
                         type="text"
                         placeholder="Digite o nome do instrumento"
                         value={instrumentName}
@@ -65,23 +103,54 @@ const Instrument_Register = () => {
                     <Form.Group>
                       <Form.Label className='mb-2 mt-2 '>Descrição do Instrumento</Form.Label>
                       <Form.Control
+                        required
                         as="textarea"
                         rows={3}
-                        placeholder="Digite a descrição do instrumento"
+                        placeholder="Estado de conservação, se há detalhes, se possui todos seus componentes..."
                         value={instrumentDescription}
                         onChange={(e) => setInstrumentDescription(e.target.value)}
                       />
                     </Form.Group>
-                    {errorMsg && <Alert variant="danger">{errorMsg}</Alert>}
-                    {successMsg && <Alert variant="success">{successMsg}</Alert>}
-                    <div className="d-flex justify-content-between">
-                      <Button className='mb-2 mt-4' size='sm' onClick={() => window.history.back()}>
-                        <ArrowLeft size={18} className="me-1" />Voltar
-                      </Button>
-                      <Button className='mb-2 mt-4' type="submit">Cadastrar</Button>
+                    {errorMsg && <Alert dismissible onClose={() => setErrorMsg(false)} variant="danger">{errorMsg}</Alert>}
+                    {successMsg && <Alert dismissible onClose={() => setSuccessMsg(false)} className='mt-4' variant="success">{successMsg}!</Alert>}
+
+                    <div className="text-right">
+                      {!studioInstruments.length ? (
+                        <Button className='mb-2 mt-4 bg-success btn-sm' type="submit">
+                          Cadastrar Instrumento
+                        </Button>
+                      ) : (
+                        <>
+                          <Button className='mb-2 mt-4 bg-success btn-sm me-2' type="submit">
+                            Cadastrar Novo Instrumento
+                          </Button>
+                        </>
+                      )}
                     </div>
                   </form>
                 </div>
+                {studioInstruments.length ? (
+                  <div className='h6'>Instrumentos cadastrados:</div>
+                ) : null}
+                <ul className="list-group">
+                  {studioInstruments.map((instrument, key) => (
+                    <li key={key} className="list-group-item d-flex justify-content-between align-items-start">
+                      <div className="ms-2 me-auto">
+                        <div className="fw-bold">{instrument.name}</div>
+                        {instrument.description}
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+                {studioInstruments.length ? (
+                  <div className='text-right'>
+                    <Link to={`/detalhes-estudio/${estudioId}`}>
+                      <Button className='mb-2 mt-4 bg-primary btn-block fw-bold' type="submit">
+                        Finalizar Cadastro <Check className='ms-1' />
+                      </Button>
+                    </Link>
+                  </div>
+                ) : null}
               </Card.Body>
             </Card>
           </Col>
--- front-end/src/pages/studio-register/index.js
+++ front-end/src/pages/studio-register/index.js
@@ -1,16 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Col, Button, Row, Container, Card, Form } from 'react-bootstrap';
+import { Col, Button, Row, Container, Card, Form, Badge } from 'react-bootstrap';
 import axios from 'axios';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 import { Alert } from 'react-bootstrap';
 import Navigation from '../../components/Navigation';
+import { ChevronRight } from 'react-feather';
+import { toast } from "react-toastify";
 
 export function Studio_Register() {
 
   const navigate = useNavigate();
   const [errorMsg, setErrorMsg] = useState('');
-  const [successMsg, setSuccessMsg] = useState('');
 
   const getErrorMsg = () => {
     if (typeof errorMsg === 'string' || errorMsg instanceof String) return errorMsg;
@@ -34,8 +35,7 @@ export function Studio_Register() {
         'Accept': 'application/json',
       }
     }).then((res) => {
-      console.log(res.data);
-      setSuccessMsg('Estudio cadastrado com sucesso');
+      toast.success('Estúdio criado com sucesso!');
       navigate(`/estudios/:${res.data.id}/cadastrar-instrumentos`);
     }).catch(error => {
       console.error(error);
@@ -52,107 +52,118 @@ export function Studio_Register() {
 
   return (
 
-    <>
-      <div style={{ background: "#0E243B" }}>
-        <Container>
-          <Navigation />
-          <Row className=" vh-100 d-flex justify-content-center align-items-center">
-            <Col md={8} lg={6} xs={12}>
-              <Card className="px-4 mt-5">
-                <Card.Body>
-                  <form onSubmit={onSubmitFunction}>
-                    <div className="mb-3 mt-md-4">
-                      <h2 className="fw-bold mb-2 text-center text-uppercase ">
-                        Cadastre seu Studio
-                      </h2>
-                      <div className="mb-3">
-                        <Form.Label className="mb-2 text-center">
-                          Nome do Studio
-                        </Form.Label>
-                        <Form.Control
-                          className="mb-2"
-                          type="text"
-                          required="true"
-                          placeholder="Coloque o nome"
-                          value={studioName}
-                          onChange={(e) => setStudioName(e.target.value)}
-                        />
-                        <Form.Group
-                          className="mb-3"
-                          controlId="nomeDoStudio"
-                        >
-                          <Form.Label className="mb-1 mt-2 text-center">
-                            Endereço
-                          </Form.Label>
-                          <Form.Control
-                            className="mb-3"
-                            type="text"
-                            placeholder="Cidade"
-                            value={address}
-                            required="true"
-                            onChange={(e) => setAddress(e.target.value)}
-                          />
-                          <Form.Control
-                            className="mb-3"
-                            type="text"
-                            placeholder="CEP"
-                            required="true"
-                            value={zipCode}
-                            onChange={(e) => setZipCode(e.target.value)}
-                          />
-                          <Form.Control
-                            className="mb-3"
-                            type="number"
-                            placeholder="Numero"
-                            required="true"
-                            value={number}
-                            onChange={(e) => setNumber(e.target.value)}
-                          />
+    <div style={{ background: "#0E243B" }}>
+      <Container>
+        <Navigation />
+        <Row className="d-flex justify-content-center align-items-center">
+          <Col md={8} lg={6} xs={12}>
+            <Card className="px-sm-4 mt-5">
+              <Card.Body>
+                <form onSubmit={onSubmitFunction}>
+                  <div className="mb-3 mt-md-4">
+                    <h3 className="fw-bold mb-2 text-center text-uppercase ">
+                      Cadastre seu <span className='text-primary'>Estúdio</span>
+                    </h3>
+                    <div className='text-center my-3'>
+                      <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
+                        <input type="radio" className="btn-check cursor-normal" name="btnradio" id="btnradio1" autocomplete="off" checked />
+                        <label className="btn btn-sm btn-outline-primary cursor-normal" for="btnradio1">
+                          1 - Informações do estúdio
+                        </label>
 
-                          <Form.Control className="mb-3"
-                            type="text"
-                            placeholder="Bairro"
-                            required="true"
-                            value={neighborhood}
-                            onChange={(e) => setNeighborhood(e.target.value)}
-                          />
-                        </Form.Group>
-                        <Form.Group
-                          className="mb-3"
-                          controlId="enderecoStudio"
-                        >
-                        </Form.Group>
-                        <Form.Label className=" mb-3 text-center">
-                          Descrição
-                        </Form.Label>
-                        <textarea className="mb-3 form-control" rows="5"
-                          placeholder='Descreva o seu estudio'
-                          value={description}
-                          onChange={(e) => setDescription(e.target.value)}
-                        ></textarea>
-                        {errorMsg ? (
-                          <Alert variant='danger' className='mx-2'>
-                            {getErrorMsg()}
-                          </Alert>
-                        ) : null}
-                        <button type='submit'
-                          className="btn btn-lg btn-primary w-100 fs-6"
-                          style={{ background: '#546CCF' }} >
-                          Cadastrar estúdio
-                        </button>
-                        <div className="small fst-italic text-center mt-1" style={{ color: 'rgba(0,0,0,0.5)' }}>
-                          Em seguida, você será redirecionado p/ adicionar instrumentos ao estúdio
-                        </div>
+                        <input type="radio" className="btn-check" disabled name="btnradio" id="btnradio2" autocomplete="off" />
+                        <label className="btn btn-sm btn-outline-primary btn-disabled" for="btnradio2">
+                          2 - Instrumentos
+                        </label>
                       </div>
                     </div>
-                  </form>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    </>
+                    <div className="mb-3">
+                      <Form.Label className="mb-2 text-center">
+                        Nome do Studio
+                      </Form.Label>
+                      <Form.Control
+                        className="mb-2"
+                        type="text"
+                        required="true"
+                        placeholder="Coloque o nome"
+                        value={studioName}
+                        onChange={(e) => setStudioName(e.target.value)}
+                      />
+                      <Form.Group
+                        className="mb-3"
+                        controlId="nomeDoStudio"
+                      >
+                        <Form.Label className="mb-1 mt-2 text-center">
+                          Endereço
+                        </Form.Label>
+                        <Form.Control
+                          className="mb-3"
+                          type="text"
+                          placeholder="Cidade"
+                          value={address}
+                          required="true"
+                          onChange={(e) => setAddress(e.target.value)}
+                        />
+                        <Form.Control
+                          className="mb-3"
+                          type="text"
+                          placeholder="CEP"
+                          required="true"
+                          value={zipCode}
+                          onChange={(e) => setZipCode(e.target.value)}
+                        />
+                        <Form.Control
+                          className="mb-3"
+                          type="number"
+                          placeholder="Numero"
+                          required="true"
+                          value={number}
+                          onChange={(e) => setNumber(e.target.value)}
+                        />
+
+                        <Form.Control className="mb-3"
+                          type="text"
+                          placeholder="Bairro"
+                          required="true"
+                          value={neighborhood}
+                          onChange={(e) => setNeighborhood(e.target.value)}
+                        />
+                      </Form.Group>
+                      <Form.Group
+                        className="mb-3"
+                        controlId="enderecoStudio"
+                      >
+                      </Form.Group>
+                      <Form.Label className=" mb-3 text-center">
+                        Descrição
+                      </Form.Label>
+                      <textarea className="mb-3 form-control" rows="5"
+                        placeholder='Descreva o seu estudio'
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                      ></textarea>
+                      {errorMsg ? (
+                        <Alert variant='danger' className='mx-2'>
+                          {getErrorMsg()}
+                        </Alert>
+                      ) : null}
+                      <button type='submit'
+                        className="btn btn-lg btn-primary w-100 fs-6 d-flex align-items-center justify-content-center"
+                        style={{ background: '#546CCF' }} >
+                        Cadastrar estúdio <ChevronRight className='ms-1' />
+                      </button>
+                      <div className="small fst-italic text-center mt-1" style={{ color: 'rgba(0,0,0,0.5)' }}>
+                        Em seguida, você será redirecionado p/ adicionar instrumentos ao estúdio
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
 
